96727817eaf32935e0d05342f0bddadea4941d3d
Wouter Bolhuis <wouter.bolhuis@gmail.com>
2016-01-20 13:13:48 +0100

    Fixed stupidstrategy
    Made stupid strategy a bit better and simpler

--- src/qwirkle/strategy/StupidStrategy.java
+++ src/qwirkle/strategy/StupidStrategy.java
@@ -3,9 +3,8 @@ package qwirkle.strategy;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+import java.util.Set;
 
-import qwirkle.core.Board;
 import qwirkle.core.Game;
 import qwirkle.core.Tile;
 
@@ -14,32 +13,16 @@ public class StupidStrategy implements Strategy {
 	@Override
 	public Map<String, Tile> determineMove(Game game, ArrayList<Tile> hand) {
 		Map<String, Tile> toReturn = new HashMap<String, Tile>();
-		Random rand = new Random();
-		int tile = rand.nextInt(6);
-		String coords = Board.makeString(0,0);
-		if(game.getBoard().getTiles().isEmpty()){
-			toReturn.put(coords, hand.get(tile));
-		} else {
-			boolean ready = false;
-			while(!ready){
-				tile = rand.nextInt(6);
-				coords = generateRandomCoords(game);
-				if(game.getRules().isMoveAllowed(coords, hand.get(tile))){
-					ready = true;
+		Set<String> emptyCoords = SmartStrategy.emptyCoords(game);
+		for(String coords: emptyCoords){
+			for(Tile tile : hand){
+				if(game.getRules().isMoveAllowed(coords, tile)){
+					toReturn.put(coords, tile);
 				}
+				toReturn.put(coords, tile);
 			}
 		}
-		toReturn.put(coords, hand.get(tile));
 		return toReturn;
 	}
 
-	public String generateRandomCoords(Game game) {
-		int[] limits = game.getBoard().getLimits(game.getBoard().getTiles());
-       	Random rand = new Random();
-		int x = rand.nextInt(Math.max(-1*limits[0], limits[2]));
-		int y = rand.nextInt(Math.max(-1*limits[1], limits[3]));
-        x *= (-1)^rand.nextInt(2);
-        y *= (-1)^rand.nextInt(2);
-        return Board.makeString(x, y);
-    }
 }
