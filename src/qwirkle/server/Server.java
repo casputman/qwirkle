085b84e83dd26913fbacd16244cc115afe0e34a3
Cas Putman <cas-putman@Live.nl>
2016-01-24 23:43:20 +0100

    More protocolresponses

--- src/qwirkle/server/Server.java
+++ src/qwirkle/server/Server.java
@@ -171,238 +171,258 @@ public class Server {
 					}
 				}
 			}
-		}
-	}
-
-
-	/**
-	 * send ProtocolControl.acceptRequest to the handler
-	 * @param handler
-	 * @param mark
-	 */
-	private synchronized void acceptRequest(ClientHandler handler) {
-		sendMessage(handler, Protocol.SERVER_CORE_JOIN_ACCEPTED 
-				+ Protocol.MESSAGESEPERATOR + handler.getName());
-	}
-
-	/**
-	 * Whenever a new client connects the handler assigned to that client gets added
-	 * to the handler list threads
-	 * @param handler
-	 */
-	//@ requires !threads.contains(handler);
-	public synchronized void addHandler(ClientHandler handler) {
-		threads.add(handler);
-	}
-
-	/**
-	 * Whenever a clients disconnects the clients ClientHandler gets remove 
-	 * from whatever list it was in
-	 * @param handler
-	 */
-	//@ requires threads.contains(handler) || joined.contains(handler) || playing.contains(handler);
-	/*@ ensures !threads.contains(handler) || !joined.contains(handler) 
-    || !playing.contains(handler);@*/
-	public synchronized void removeHandler(ClientHandler handler) {
-		if (threads.contains(handler)) {
-			threads.remove(handler);
-		} else if (playing.contains(handler)) {
-			playing.remove(handler);
-		} else if (joined.contains(handler)) {
-			joined.remove(handler);
-		}
-		System.out.println("\n   DISCONNECTING: " + handler.getClientName()
-		+ " has left the game\n");
-		sendMessage(handler2(handler), Protocol.SERVER_CORE_GAME_ENDED
-				+ Protocol.MESSAGESEPERATOR
-				+ handler.getClientName() + Protocol.MESSAGESEPERATOR
-				+ Protocol.SERVER_CORE_DISCONNECT);
-	}
-
-	/**
-	 * Checks whether the client requesting the move is the client which should be making a move
-	 * If not it will send that client 
-	 * ProtocolConstants.invalidCommand ProtocolControl.invalidUserTurn
-	 * If it is it will check if it is a valid move
-	 * To do so it will first change the index to the respective column and then check
-	 * if there is still space left in this column
-	 * If there is no space left it will return 
-	 * ProtocolConstants.invalidMove ProtocolConstants.invalidMove
-	 * if there is space left it will make the move 
-	 * and return the ProtocolControl.moveResult command to the client
-	 * @param handler
-	 * @param decision
-	 */
-	//@ requires handler != null;
-	public synchronized void doMove(ClientHandler handler, String coordinates){
-		String coordinatesArray[] = coordinates.split(Protocol.MESSAGESEPERATOR);
-		int xCoordinate = Integer.parseInt(coordinatesArray[0]);
-		int yCoordinate = Integer.parseInt(coordinatesArray[1]);
-		int shape = Integer.parseInt(coordinatesArray[2]);
-		int color = Integer.parseInt(coordinatesArray[3]);
-		Tile tile = handler.getGame().getBoard().makeTile(shape, color);
-		boolean moveAllowed = false;
-		if (handler.getGame().current.equals(handler.getPlayer())) {
-			moveAllowed = (handler.getGame().getRules().isMoveAllowed(coordinates, tile)); {
+		} else if (input[0].equals(Protocol.CLIENT_CORE_DONE)){
+			for (int i = 0; i < joined.size(); i++){
+				sendMessage(joined.get(i), "score");
 			}
 
-			if (moveAllowed) {
-				handler.getGame().takeTurn(xCoordinate, yCoordinate, tile);
-				ClientHandler[] doMove = gameMap.get(handler.getGame());
-				if (!handler.getGame().getRunning()) {
-					gameEnded(handler.getGame());
-				}
-				for (ClientHandler handle : doMove) {
-					sendMessage(handle, Protocol.SERVER_CORE_MOVE_ACCEPTED);
-					sendMessage(handle, Protocol.SERVER_CORE_MOVE_MADE 
-							+ Protocol.MESSAGESEPERATOR
-							+ xCoordinate
-							+ Protocol.MESSAGESEPERATOR
-							+ yCoordinate
-							+ Protocol.MESSAGESEPERATOR
-							+ tile);
-				}
-			} else {
-				sendMessage(handler, Protocol.CLIENT_CORE_DONE
-						+ Protocol.MESSAGESEPERATOR + Protocol.SERVER_CORE_MOVE_DENIED
-						+ Protocol.MESSAGESEPERATOR + handler.getClientName());
+		} else if (input[0].equals(Protocol.CLIENT_CORE_SWAP)){
+			ArrayList<Tile> hand = handler.getPlayer().getHand();
+			int shape = Integer.parseInt(input[1]);
+			int colour = Integer.parseInt(input[2]);
+			Tile tile  = handler.getGame().getBoard().makeTile(shape, colour);
+			if (!(handler.getGame().getBag().tileBag.containsValue(1)) || !(handler.getGame().getBag().tileBag.containsValue(2)) || !(handler.getGame().getBag().tileBag.containsValue(3))  || (!hand.contains(tile))){
+				sendMessage(handler, Protocol.SERVER_CORE_SWAP_DENIED); }
+			else {
+				handler.getPlayer().addTile(handler.getGame().getBag().drawTile());
+				sendMessage(handler, Protocol.SERVER_CORE_SWAP_ACCEPTED);
+			}
+		} else if (input[0].equals(Protocol.CLIENT_CORE_EXIT)){
+			removeHandler(handler);
+		}
+	}
 
+
+
+
+/**
+ * send ProtocolControl.acceptRequest to the handler
+ * @param handler
+ * @param mark
+ */
+private synchronized void acceptRequest(ClientHandler handler) {
+	sendMessage(handler, Protocol.SERVER_CORE_JOIN_ACCEPTED 
+			+ Protocol.MESSAGESEPERATOR + handler.getName());
+}
+
+/**
+ * Whenever a new client connects the handler assigned to that client gets added
+ * to the handler list threads
+ * @param handler
+ */
+//@ requires !threads.contains(handler);
+public synchronized void addHandler(ClientHandler handler) {
+	threads.add(handler);
+}
+
+/**
+ * Whenever a clients disconnects the clients ClientHandler gets remove 
+ * from whatever list it was in
+ * @param handler
+ */
+//@ requires threads.contains(handler) || joined.contains(handler) || playing.contains(handler);
+/*@ ensures !threads.contains(handler) || !joined.contains(handler) 
+    || !playing.contains(handler);@*/
+public synchronized void removeHandler(ClientHandler handler) {
+	if (threads.contains(handler)) {
+		threads.remove(handler);
+	} else if (playing.contains(handler)) {
+		playing.remove(handler);
+	} else if (joined.contains(handler)) {
+		joined.remove(handler);
+	}
+	System.out.println("\n   DISCONNECTING: " + handler.getClientName()
+	+ " has left the game\n");
+	sendMessage(handler2(handler), Protocol.SERVER_CORE_GAME_ENDED
+			+ Protocol.MESSAGESEPERATOR 
+			+ handler.getClientName() + Protocol.MESSAGESEPERATOR
+			+ Protocol.SERVER_CORE_DISCONNECT);
+}
+
+/**
+ * Checks whether the client requesting the move is the client which should be making a move
+ * If not it will send that client 
+ * ProtocolConstants.invalidCommand ProtocolControl.invalidUserTurn
+ * If it is it will check if it is a valid move
+ * To do so it will first change the index to the respective column and then check
+ * if there is still space left in this column
+ * If there is no space left it will return 
+ * ProtocolConstants.invalidMove ProtocolConstants.invalidMove
+ * if there is space left it will make the move 
+ * and return the ProtocolControl.moveResult command to the client
+ * @param handler
+ * @param decision
+ */
+//@ requires handler != null;
+public synchronized void doMove(ClientHandler handler, String coordinates){
+	String coordinatesArray[] = coordinates.split(Protocol.MESSAGESEPERATOR);
+	int xCoordinate = Integer.parseInt(coordinatesArray[0]);
+	int yCoordinate = Integer.parseInt(coordinatesArray[1]);
+	int shape = Integer.parseInt(coordinatesArray[2]);
+	int color = Integer.parseInt(coordinatesArray[3]);
+	Tile tile = handler.getGame().getBoard().makeTile(shape, color);
+	boolean moveAllowed = false;
+	if (handler.getGame().current.equals(handler.getPlayer())) {
+		moveAllowed = (handler.getGame().getRules().isMoveAllowed(coordinates, tile)); {
+		}
+
+		if (moveAllowed) {
+			handler.getGame().takeTurn(xCoordinate, yCoordinate, tile);
+			ClientHandler[] doMove = gameMap.get(handler.getGame());
+			if (!handler.getGame().getRunning()) {
+				gameEnded(handler.getGame());
+			}
+			for (ClientHandler handle : doMove) {
+				sendMessage(handle, Protocol.SERVER_CORE_MOVE_ACCEPTED);
+				sendMessage(handle, Protocol.SERVER_CORE_MOVE_MADE 
+						+ Protocol.MESSAGESEPERATOR
+						+ xCoordinate
+						+ Protocol.MESSAGESEPERATOR
+						+ yCoordinate
+						+ Protocol.MESSAGESEPERATOR
+						+ tile);
 			}
 		} else {
-			sendMessage(handler,
-					Protocol.SERVER_CORE_DENIED + Protocol.MESSAGESEPERATOR
-					+ Protocol.SERVER_CORE_TURN + Protocol.MESSAGESEPERATOR
-					+ (handler.getGame().current.getName()));
-		}
-	}
+			sendMessage(handler, Protocol.CLIENT_CORE_DONE
+					+ Protocol.MESSAGESEPERATOR + Protocol.SERVER_CORE_MOVE_DENIED
+					+ Protocol.MESSAGESEPERATOR + handler.getClientName());
 
-	/**
-	 * Checks whether the game has ended and sends the ProtocolControl.endGame command 
-	 * with the correct results to both participating clients 
-	 * @param game
-	 */
-	//@ requires !game.getRunning();
-	private void gameEnded(Game game) {
-		ClientHandler[] clients = gameMap.get(game);
-		if (!game.getRunning()) {
-			if (game.getRules().hasWinner()) {
-				for (ClientHandler client : clients) {
-					if (game.getRules().isWinner(client.getPlayer())) {
-						sendMessage(client, Protocol.SERVER_CORE_GAME_ENDED
-								+ Protocol.MESSAGESEPERATOR + client.getClientName()
-								+ Protocol.MESSAGESEPERATOR + ProtocolConstants.winner);
-						sendMessage(handler2(client), Protocol.SERVER_CORE_GAME_ENDED
-								+ Protocol.MESSAGESEPERATOR + client.getClientName()
-								+ Protocol.MESSAGESEPERATOR + ProtocolConstants.winner);
-					}
+		}
+	} else {
+		sendMessage(handler,
+				Protocol.SERVER_CORE_DENIED + Protocol.MESSAGESEPERATOR
+				+ Protocol.SERVER_CORE_TURN + Protocol.MESSAGESEPERATOR
+				+ (handler.getGame().current.getName()));
+	}
+}
+
+/**
+ * Checks whether the game has ended and sends the ProtocolControl.endGame command 
+ * with the correct results to both participating clients 
+ * @param game
+ */
+//@ requires !game.getRunning();
+private void gameEnded(Game game) {
+	ClientHandler[] clients = gameMap.get(game);
+	if (!game.getRunning()) {
+		if (game.getRules().hasWinner()) {
+			for (ClientHandler client : clients) {
+				if (game.getRules().isWinner(client.getPlayer())) {
+					sendMessage(client, Protocol.SERVER_CORE_GAME_ENDED
+							+ Protocol.MESSAGESEPERATOR + client.getClientName()
+							+ Protocol.MESSAGESEPERATOR + ProtocolConstants.winner);
+					sendMessage(handler2(client), Protocol.SERVER_CORE_GAME_ENDED
+							+ Protocol.MESSAGESEPERATOR + client.getClientName()
+							+ Protocol.MESSAGESEPERATOR + ProtocolConstants.winner);
 				}
-			} else {
-				for (ClientHandler client : clients) {
-					sendMessage(client, Protocol.SERVER_CORE_GAME_ENDED + Protocol.MESSAGESEPERATOR
-							+ client.getClientName() + Protocol.MESSAGESEPERATOR
-							+ ProtocolConstants.draw);
-				}
+			}
+		} else {
+			for (ClientHandler client : clients) {
+				sendMessage(client, Protocol.SERVER_CORE_GAME_ENDED + Protocol.MESSAGESEPERATOR
+						+ client.getClientName() + Protocol.MESSAGESEPERATOR
+						+ ProtocolConstants.draw);
 			}
 		}
 	}
+}
 
-	/**
-	 * returns the player who is currently making a turn
-	 * @param handler
-	 */
-	//@pure
-	public synchronized void turn(ClientHandler handler) {
-		sendMessage(handler,
-				Protocol.SERVER_CORE_TURN + Protocol.MESSAGESEPERATOR + handler.getGame()
-				.current.getName());
-	}
+/**
+ * returns the player who is currently making a turn
+ * @param handler
+ */
+//@pure
+public synchronized void turn(ClientHandler handler) {
+	sendMessage(handler,
+			Protocol.SERVER_CORE_TURN + Protocol.MESSAGESEPERATOR + handler.getGame()
+			.current.getName());
+}
 
-	/**
-	 * Closes the socket and terminates the server
-	 */
-	//@ ensures sock.isClosed();
-	public void shutdown() {
-		System.err.println("          STOPPING SERVER");
-		try {
-			sock.close();
-		} catch (IOException e) {
-			System.exit(0);
-		}
+/**
+ * Closes the socket and terminates the server
+ */
+//@ ensures sock.isClosed();
+public void shutdown() {
+	System.err.println("          STOPPING SERVER");
+	try {
+		sock.close();
+	} catch (IOException e) {
 		System.exit(0);
 	}
+	System.exit(0);
+}
 
-	/**
-	 * Prints the String variable and returns the user input
-	 * @param variable
-	 * @return
-	 */
-	//@ensures \result != "";
-	public String getInput(String variable) {
-		String input = null;
-		try {
-			if (variable != "") {
-				System.out.println(variable);
-			}
-			input = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-			if (input.equalsIgnoreCase("exit")) {
-				shutdown();
-			}
-
-		} catch (IOException e) {
-			System.err.println("Something went wrong");
-			System.out.println("But you know shit happens");
+/**
+ * Prints the String variable and returns the user input
+ * @param variable
+ * @return
+ */
+//@ensures \result != "";
+public String getInput(String variable) {
+	String input = null;
+	try {
+		if (variable != "") {
+			System.out.println(variable);
+		}
+		input = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+		if (input.equalsIgnoreCase("exit")) {
 			shutdown();
 		}
 
-		return input;
-
+	} catch (IOException e) {
+		System.err.println("Something went wrong");
+		System.out.println("But you know shit happens");
+		shutdown();
 	}
 
-	/**
-	 * Sends the String message to the ClientHandler handler
-	 * @param handler
-	 * @param message
-	 */
-	//@pure
-	public void sendMessage(ClientHandler handler, String message) {
-		if (joined.contains(handler) || playing.contains(handler) 
-				|| threads.contains(handler)) {
-			System.err.println("    SENDING TO " + handler.getClientName() + ": " + message);
-			handler.sendMessage(message);
-		}
-	}
+	return input;
 
-	/**
-	 * Returns the other handler
-	 * @param handler
-	 * @return
-	 */
-	//@ requires gameMap.get(handler.getGame())[0] == handler;
-	//@ ensures \result == gameMap.get(handler.getGame())[1];
-	//@ also
-	//@ requires gameMap.get(handler.getGame())[0] != handler;
-	//@ ensures \result == gameMap.get(handler.getGame())[0];
-	//@pure
-	public ClientHandler handler2(ClientHandler handler) {
-		ClientHandler[] handlers = gameMap.get(handler.getGame());
-		ClientHandler toReturn = null;
-		int player = 0;
-		if(handlers[player].equals(handler) || !handler.equals(null)){
-			player += 1;
-			player %= 4;
-			toReturn = handlers[player];
-		} else if (handler.equals(null)){
-			int random = (int )(Math.random() * 4 + 1);
-			toReturn = handlers[random];
-		} else {
-			for (int i = 0; i < handlers.length; i++){
-				if(handlers[i].equals(handler)){
-					i += 1;
-					i %= 4;
-					toReturn = handlers[i];
-				}
+}
+
+/**
+ * Sends the String message to the ClientHandler handler
+ * @param handler
+ * @param message
+ */
+//@pure
+public void sendMessage(ClientHandler handler, String message) {
+	if (joined.contains(handler) || playing.contains(handler) 
+			|| threads.contains(handler)) {
+		System.err.println("    SENDING TO " + handler.getClientName() + ": " + message);
+		handler.sendMessage(message);
+	}
+}
+
+/**
+ * Returns the other handler
+ * @param handler
+ * @return
+ */
+//@ requires gameMap.get(handler.getGame())[0] == handler;
+//@ ensures \result == gameMap.get(handler.getGame())[1];
+//@ also
+//@ requires gameMap.get(handler.getGame())[0] != handler;
+//@ ensures \result == gameMap.get(handler.getGame())[0];
+//@pure
+public ClientHandler handler2(ClientHandler handler) {
+	ClientHandler[] handlers = gameMap.get(handler.getGame());
+	ClientHandler toReturn = null;
+	int player = 0;
+	if(handlers[player].equals(handler) || !handler.equals(null)){
+		player += 1;
+		player %= 4;
+		toReturn = handlers[player];
+	} else if (handler.equals(null)){
+		int random = (int )(Math.random() * 4 + 1);
+		toReturn = handlers[random];
+	} else {
+		for (int i = 0; i < handlers.length; i++){
+			if(handlers[i].equals(handler)){
+				i += 1;
+				i %= 4;
+				toReturn = handlers[i];
 			}
 		}
-		return toReturn;
 	}
+	return toReturn;
+}
 }
